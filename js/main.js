33cb6cd32edb518b811077dc998c949f4cfc0432
deivis17 <marchandeivis@gmail.com>
2023-01-19 11:22:40 -0400

    Project Finished

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const navMin = document.querySelector('.nav-min');
 btn.addEventListener('click',()=>{
     navMin.classList.toggle('show');
 })
+
+ScrollReveal().reveal('.container', {delay:100});
+ScrollReveal().reveal('.slideshow', {delay:400});
+ScrollReveal().reveal('.grid',{delay:400});
+ScrollReveal().reveal('.social',{delay:400});
+ScrollReveal().reveal('.links',{delay:400});
